9d82ccb445718b9d27d936b56f546fa5c8d2f654
gogetto1 <ldaczz@gmail.com>
2021-01-24 22:16:16 +0100

    Update controller rspec

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 
 RSpec.describe WordsController, type: :controller do
   describe "GET index" do
-    context 'when some words present'
+    context 'when some words present' do
       it "assigns @words" do
-        word = Word.create
+        word = Word.create(value: 'cat', language: 'english')
         get :index
         expect(assigns(:words)).to eq([word])
       end
+    end
     
 
-    context 'when no words'
+    context 'when no words' do
       it "assigns @words" do
         get :index
         expect(assigns(:words)).to eq([])
       end
+    end
     
 
     it "renders the index template" do
