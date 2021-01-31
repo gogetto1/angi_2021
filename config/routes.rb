47d578fabdc412616aeac4e09e9a6d96d8946537
gogetto1 <ldaczz@gmail.com>
2021-01-31 18:50:39 +0100

    Add edit and update actions in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'words#index'
-  resources :words, only: [:index, :new, :create, :show]
+  resources :words, only: [:index, :new, :create, :show, :edit, :update]
 end
