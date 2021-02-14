0c426d624b3fffb59d7d045c23c7b09b78098b49
gogetto1 <ldaczz@gmail.com>
2021-02-14 11:53:17 +0100

    Create translation table and spec

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,5 +3,10 @@ class Word < ApplicationRecord
   belongs_to :user
   validates :content, :language, presence: true
 
+  has_many :translations_association, class_name: 'Translation'
+  has_many :translations, through: :translations_association, source: :translated_word
+  has_many :inverse_translations_association, class_name: "Translation", foreign_key: "translated_word_id"
+  has_many :inverse_translations, through: :inverse_translations_association, source: :word
+
   paginates_per 10
 end
