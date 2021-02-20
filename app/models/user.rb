607bcfc237757369cb4c974ce19f1bae5757c55f
gogetto1 <ldaczz@gmail.com>
2021-02-20 18:53:46 +0100

    Hide part of email address form user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :words
+
+  def secret_email
+    email.gsub(/.{0,4}@/, '####@')
+  end
 end
