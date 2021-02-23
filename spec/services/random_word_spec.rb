8a22d8072a8117e3e94e74b3a3c0bbf6a86d51ad
gogetto1 <ldaczz@gmail.com>
2021-02-23 20:55:12 +0100

    Query only words with translations when looking for random words

--- spec/services/random_word_spec.rb
+++ spec/services/random_word_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
 
 describe Words::RandomWord do
-  subject { described_class }
-
   describe '#call' do
     subject { described_class.new.call }
 
-    let!(:word) { create(:word) }
+    context 'When word deos not have any translations' do
+      let!(:word) { create(:word) }
 
-    it do
-      expect(subject).to eq(word)
+      it do
+        expect(subject).to eq(nil)
+      end
+    end
+
+    context 'When word has some translations' do
+      let!(:word) { create(:word) }
+
+      before do
+        word.translations << create(:word)
+      end
+
+      it do
+        expect(subject).to eq(word)
+      end
     end
   end
-
-
 end
